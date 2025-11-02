c2ae646e94f5a482df754f07575d3a5e37dba3f0
clouddevopstrainer <clouddevopstrainer@outlook.com>
2025-11-02 14:54:37 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,8 +5,8 @@ provider "aws" {
 # ------------------------------
 # Security Group
 # ------------------------------
-resource "aws_security_group" "devnw20_sg" {
-  name        = "devnw20_sg"
+resource "aws_security_group" "devnw21_sg" {
+  name        = "devnw21_sg"
   description = "Allow SSH, HTTP, NodePort, Prometheus, Grafana, Node Exporter"
 
   ingress {
@@ -65,18 +65,18 @@ resource "aws_security_group" "devnw20_sg" {
   }
 
   tags = {
-    Name = "devnw20_sg"
+    Name = "devnw21_sg"
   }
 }
 
 # ------------------------------
 # EC2 Instance
 # ------------------------------
-resource "aws_instance" "app20_servernew" {
+resource "aws_instance" "app21_servernew" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   key_name               = var.key_name
-  vpc_security_group_ids = [aws_security_group.devnw20_sg.id]
+  vpc_security_group_ids = [aws_security_group.devnw21_sg.id]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -116,7 +116,7 @@ resource "aws_instance" "app20_servernew" {
               EOF
 
   tags = {
-    Name = "app20_servernew"
+    Name = "app21_servernew"
   }
 }
 
@@ -124,5 +124,5 @@ resource "aws_instance" "app20_servernew" {
 # Output
 # ------------------------------
 output "instance_public_ip" {
-  value = aws_instance.app20_servernew.public_ip
+  value = aws_instance.app21_servernew.public_ip
 }
